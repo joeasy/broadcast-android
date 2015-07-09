b0ea6f79e18a0b845e21b1a9b3f226190a03b690
basagee <basagee@gmail.com>
2015-07-09 17:56:37 +0900

    web view onReceivedError 처리

--- common_library/src/main/java/com/nbplus/hybrid/BasicWebViewClient.java
+++ common_library/src/main/java/com/nbplus/hybrid/BasicWebViewClient.java
@@ -224,31 +224,31 @@ public class BasicWebViewClient extends WebViewClient {
     public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
         mPageLoadSuccess = false;
         Log.e(TAG, "WebView client onReceivedError code = " + errorCode);
-//        switch (errorCode) {
-//            case ERROR_AUTHENTICATION:               // 서버에서 사용자 인증 실패
-//            case ERROR_BAD_URL:                            // 잘못된 URL
-//            case ERROR_CONNECT:                           // 서버로 연결 실패
-//            case ERROR_FAILED_SSL_HANDSHAKE:     // SSL handshake 수행 실패
-//            case ERROR_FILE:                                   // 일반 파일 오류
-//            case ERROR_FILE_NOT_FOUND:                // 파일을 찾을 수 없습니다
-//            case ERROR_HOST_LOOKUP:            // 서버 또는 프록시 호스트 이름 조회 실패
-//            case ERROR_IO:                               // 서버에서 읽거나 서버로 쓰기 실패
-//            case ERROR_PROXY_AUTHENTICATION:    // 프록시에서 사용자 인증 실패
-//            case ERROR_REDIRECT_LOOP:                // 너무 많은 리디렉션
-//            case ERROR_TIMEOUT:                          // 연결 시간 초과
-//            case ERROR_TOO_MANY_REQUESTS:            // 페이지 로드중 너무 많은 요청 발생
-//            case ERROR_UNKNOWN:                         // 일반 오류
-//            case ERROR_UNSUPPORTED_AUTH_SCHEME:  // 지원되지 않는 인증 체계
-//            case ERROR_UNSUPPORTED_SCHEME:
-//                AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
-//                builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
-//                    public void onClick(DialogInterface dialog, int which) {
-//                        closeWebApplication();
-//                    }
-//                });
-//                builder.setMessage(R.string.webview_page_loading_error);
-//                builder.show();
-//        }
+        switch (errorCode) {
+            case ERROR_AUTHENTICATION:               // 서버에서 사용자 인증 실패
+            case ERROR_BAD_URL:                            // 잘못된 URL
+            case ERROR_CONNECT:                           // 서버로 연결 실패
+            case ERROR_FAILED_SSL_HANDSHAKE:     // SSL handshake 수행 실패
+            case ERROR_FILE:                                   // 일반 파일 오류
+            case ERROR_FILE_NOT_FOUND:                // 파일을 찾을 수 없습니다
+            case ERROR_HOST_LOOKUP:            // 서버 또는 프록시 호스트 이름 조회 실패
+            case ERROR_IO:                               // 서버에서 읽거나 서버로 쓰기 실패
+            case ERROR_PROXY_AUTHENTICATION:    // 프록시에서 사용자 인증 실패
+            case ERROR_REDIRECT_LOOP:                // 너무 많은 리디렉션
+            case ERROR_TIMEOUT:                          // 연결 시간 초과
+            case ERROR_TOO_MANY_REQUESTS:            // 페이지 로드중 너무 많은 요청 발생
+            case ERROR_UNKNOWN:                         // 일반 오류
+            case ERROR_UNSUPPORTED_AUTH_SCHEME:  // 지원되지 않는 인증 체계
+            case ERROR_UNSUPPORTED_SCHEME:
+                AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        closeWebApplication();
+                    }
+                });
+                builder.setMessage(R.string.webview_page_loading_error);
+                builder.show();
+        }
 
         // Default behaviour
         super.onReceivedError(view, errorCode, description, failingUrl);
