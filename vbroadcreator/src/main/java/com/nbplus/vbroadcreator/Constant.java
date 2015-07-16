38db3f1f6fc3b6198ccb414ad8a357cd2fe9649f
basagee <basagee@gmail.com>
2015-07-16 13:34:03 +0900

    송출클라이언트 추가

---
+++ vbroadcreator/src/main/java/com/nbplus/vbroadcreator/Constant.java
@@ -0,0 +1,8 @@
+package com.nbplus.vbroadcreator;
+
+/**
+ * Created by basagee on 2015. 7. 16..
+ */
+public class Constant {
+    public static final String BROADCAST_CREATOR_SELECT_SERVER = "http://175.207.46.132:8080//common/selectServer.snc";
+}
