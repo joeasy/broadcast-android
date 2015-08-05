03c466b459eacba52a92552a29e8accea1048a37
basagee <basagee@gmail.com>
2015-08-05 15:55:36 +0900

    원격송출 테마 및 크롬브라우저 설치 확인

--- vbroadcreator/src/main/java/com/nbplus/vbroadcreator/MainActivity.java
+++ vbroadcreator/src/main/java/com/nbplus/vbroadcreator/MainActivity.java
@@ -1,9 +1,12 @@
 package com.nbplus.vbroadcreator;
 
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,11 +18,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-
-        Intent intent = new Intent(Intent.ACTION_VIEW).setData(Uri.parse(Constant.BROADCAST_CREATOR_SELECT_SERVER));
-        startActivity(intent);
-
-        finish();
+        try {
+            Intent intent = new Intent(Intent.ACTION_VIEW)
+                    .addCategory(Intent.CATEGORY_BROWSABLE)
+                    .setPackage("com.android.chrome")           // open only chrome
+                    .setData(Uri.parse(Constant.BROADCAST_CREATOR_SELECT_SERVER));
+            startActivity(intent);
+            finish();
+        } catch (ActivityNotFoundException e) {
+            e.printStackTrace();
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setPositiveButton(R.string.alert_ok, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                    finish();
+                }
+            });
+            alert.setNegativeButton(R.string.alert_market, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse("market://details?id=com.android.chrome"));
+                    startActivity(intent);
+                    finish();
+                }
+            });
+            alert.setMessage(R.string.activity_not_found_exception);
+            alert.show();
+        }
     }
 
     @Override
