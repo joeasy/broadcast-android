9834d1d97dafeb199404e42d98a30f2bd96a6c63
basagee <basagee@gmail.com>
2015-10-07 17:59:04 +0900

    로그 및 원격수신 플레이서비스 체크 수정

--- launcher/src/main/java/com/nbplus/vbroadlauncher/widget/TextClock.java
+++ launcher/src/main/java/com/nbplus/vbroadlauncher/widget/TextClock.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 The Android Open Source Project
+ * Copyright (c) 2015. NB Plus (www.nbplus.co.kr)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,6 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
 package com.nbplus.vbroadlauncher.widget;
